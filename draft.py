1c1cd4efeadf2be24b86d783ded95aa430c94c99
Oleg Ishchenko <121249280+bugab0o@users.noreply.github.com>
2023-10-24 16:48:56 +0400

    second

--- draft.py
+++ draft.py
@@ -1,62 +1,81 @@
-print("Hello, world!")
+# print("Hello, world!")
 
-strings = ["Hello", "world"]
-numbers = [1, 2, 3, 4, 5]
-data = ["hello", 1]
+# strings = ["Hello", "world"]
+# numbers = [1, 2, 3, 4, 5]
+# data = ["hello", 1]
 
-print(strings)
-print(numbers)
-print(data)
+# print(strings)
+# print(numbers)
+# print(data)
 
-summa = numbers + data
-print(summa)
+# summa = numbers + data
+# print(summa)
 
-numbers.append(6)
-print(numbers)
+# numbers.append(6)
+# print(numbers)
 
-first = strings[0]
-second = strings[1]
-print(first)
-print(second)
+# first = strings[0]
+# second = strings[1]
+# print(first)
+# print(second)
 
-strings_length = len(strings)
-print(strings_length)
+# strings_length = len(strings)
+# print(strings_length)
 
-s = sum(numbers)
-print(s)
+# s = sum(numbers)
+# print(s)
 
 
-# cat: кошка
-# bat: летучая мышь
+# # cat: кошка
+# # bat: летучая мышь
 
-# ключ : значение
+# # ключ : значение
 
-dictionary = {
-  "cat": "кошка",
-  "bat": "летучая мышь"
-}
+# dictionary = {
+#   "cat": "кошка",
+#   "bat": "летучая мышь"
+# }
 
-# print(dictionary)
-# cat = dictionary["cat"]
-# print(cat)
+# # print(dictionary)
+# # cat = dictionary["cat"]
+# # print(cat)
 
-countries = {
-  "Африка": ["Египет", "Конго", "ЮАР"],
-  "Азия": ["Китай", "Таиланд", "Индонезия"]
-}
+# countries = {
+#   "Африка": ["Египет", "Конго", "ЮАР"],
+#   "Азия": ["Китай", "Таиланд", "Индонезия"]
+# }
 
-africa = countries["Африка"]
-print(africa)
+# africa = countries["Африка"]
+# print(africa)
 
-africa_key = "Африка"
-print(countries[africa_key])
+# africa_key = "Африка"
+# print(countries[africa_key])
 
-countries["Европа"] = ["Австрия", "Испания", "Италия"]
-print(countries)
-print(countries["Европа"])
-# countries[0] = "Hello"
+# countries["Европа"] = ["Австрия", "Испания", "Италия"]
 # print(countries)
-africa.append("Эфиопия")
-print(africa)
-print(countries)
-print(len(countries["Африка"]))
+# print(countries["Европа"])
+# # countries[0] = "Hello"
+# # print(countries)
+# africa.append("Эфиопия")
+# print(africa)
+# print(countries)
+# print(len(countries["Африка"]))
+
+
+# name = input("Введите имя: ")
+# print("Привет", name)
+
+
+date, task = input("Введите дату: "), input("Введите задачу: ")
+date1, task1 = input("Введите дату: "), input("Введите задачу: ")
+date2, task2 = input("Введите дату: "), input("Введите задачу: ")
+
+# print(date, task) 
+# print(date1, task1) 
+# print(date2, task2)
+
+todolist = {date : task, 
+            date1 : task1, 
+            date2 : task2
+            }
+print(todolist)
